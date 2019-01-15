33746c33e71ae51ad6757449c674e92bc6191642
Žan Vidrih <zan@real-sec.com>
2019-01-15 11:03:35 +0100

    More types included

--- build/bc_js_noie.js
+++ build/bc_js_noie.js
@@ -1165,6 +1165,23 @@ var WalletType;
     WalletType[WalletType["ripple"] = WalletTypeConstants.BTC + 5] = "ripple";
     WalletType[WalletType["ethereum"] = WalletTypeConstants.ETH] = "ethereum";
     WalletType[WalletType["erc20Bokky"] = WalletTypeConstants.ETH | WalletTypeConstants.ERC20] = "erc20Bokky";
+    WalletType[WalletType["erc20Salt"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 1] = "erc20Salt";
+    WalletType[WalletType["erc20Polymath"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 2] = "erc20Polymath";
+    WalletType[WalletType["erc200x"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 3] = "erc200x";
+    WalletType[WalletType["erc20Cindicator"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 4] = "erc20Cindicator";
+    WalletType[WalletType["erc20CargoX"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 5] = "erc20CargoX";
+    WalletType[WalletType["erc20Viberate"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 6] = "erc20Viberate";
+    WalletType[WalletType["erc20Iconomi"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 7] = "erc20Iconomi";
+    WalletType[WalletType["erc20DTR"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 8] = "erc20DTR";
+    WalletType[WalletType["erc20OriginTrail"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 9] = "erc20OriginTrail";
+    WalletType[WalletType["erc20InsurePal"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 10] = "erc20InsurePal";
+    WalletType[WalletType["erc20Xaurum"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 11] = "erc20Xaurum";
+    WalletType[WalletType["erc20Tron"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 12] = "erc20Tron";
+    WalletType[WalletType["erc20VeChain"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 13] = "erc20VeChain";
+    WalletType[WalletType["erc20Binance"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 14] = "erc20Binance";
+    WalletType[WalletType["erc20Icon"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 15] = "erc20Icon";
+    WalletType[WalletType["erc20OmiseGo"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 16] = "erc20OmiseGo";
+    WalletType[WalletType["erc20WaltonChain"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) + 17] = "erc20WaltonChain";
     WalletType[WalletType["bitCoinTest"] = (WalletTypeConstants.BTC) | WalletTypeConstants.TESTNET] = "bitCoinTest";
     WalletType[WalletType["bitCoinCashTest"] = (WalletTypeConstants.BTC + 1) | WalletTypeConstants.TESTNET] = "bitCoinCashTest";
     WalletType[WalletType["liteCoinTest"] = (WalletTypeConstants.BTC + 2) | WalletTypeConstants.TESTNET] = "liteCoinTest";
@@ -1173,23 +1190,72 @@ var WalletType;
     WalletType[WalletType["rippleTest"] = (WalletTypeConstants.BTC + 5) | WalletTypeConstants.TESTNET] = "rippleTest";
     WalletType[WalletType["ethereumTest"] = (WalletTypeConstants.ETH) | WalletTypeConstants.TESTNET] = "ethereumTest";
     WalletType[WalletType["erc20BokkyTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20) | WalletTypeConstants.TESTNET] = "erc20BokkyTest";
+    WalletType[WalletType["erc20SaltTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 1] = "erc20SaltTest";
+    WalletType[WalletType["erc20PolymathTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 2] = "erc20PolymathTest";
+    WalletType[WalletType["erc200xTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 3] = "erc200xTest";
+    WalletType[WalletType["erc20CindicatorTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 4] = "erc20CindicatorTest";
+    WalletType[WalletType["erc20CargoXTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 5] = "erc20CargoXTest";
+    WalletType[WalletType["erc20ViberateTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 6] = "erc20ViberateTest";
+    WalletType[WalletType["erc20IconomiTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 7] = "erc20IconomiTest";
+    WalletType[WalletType["erc20DTRTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 8] = "erc20DTRTest";
+    WalletType[WalletType["erc20OriginTrailTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 9] = "erc20OriginTrailTest";
+    WalletType[WalletType["erc20InsurePalTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 10] = "erc20InsurePalTest";
+    WalletType[WalletType["erc20XaurumTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 11] = "erc20XaurumTest";
+    WalletType[WalletType["erc20TronTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 12] = "erc20TronTest";
+    WalletType[WalletType["erc20VeChainTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 13] = "erc20VeChainTest";
+    WalletType[WalletType["erc20BinanceTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 14] = "erc20BinanceTest";
+    WalletType[WalletType["erc20IconTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 15] = "erc20IconTest";
+    WalletType[WalletType["erc20OmiseGoTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 16] = "erc20OmiseGoTest";
+    WalletType[WalletType["erc20WaltonChainTest"] = (WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET) + 17] = "erc20WaltonChainTest";
 })(WalletType = exports.WalletType || (exports.WalletType = {}));
 exports.typeInfoMap = [
     { type: WalletType.bitCoin, name: "Bitcoin", ticker: "BTC" },
+    { type: WalletType.ethereum, name: "Ethereum", ticker: "ETH" },
     { type: WalletType.bitCoinCash, name: "Bitcoin Cash", ticker: "BCH" },
     { type: WalletType.liteCoin, name: "Litecoin", ticker: "LTC" },
     { type: WalletType.dash, name: "Dash", ticker: "DASH" },
     { type: WalletType.dogeCoin, name: "Dogecoin", ticker: "DOGE" },
-    { type: WalletType.ripple, name: "Ripple", ticker: "XRP" },
-    { type: WalletType.ethereum, name: "Ethereum", ticker: "ETH" },
-    { type: WalletType.erc20Bokky, name: "Bokky", ticker: "BOKKY" },
+    { type: WalletType.erc20Salt, name: "Salt", ticker: "SALT" },
+    { type: WalletType.erc20Polymath, name: "Polymath", ticker: "POLY" },
+    { type: WalletType.erc200x, name: "0X", ticker: "ZRX" },
+    { type: WalletType.erc20Cindicator, name: "Cindicator", ticker: "CND" },
+    { type: WalletType.erc20CargoX, name: "CargoX", ticker: "CXO" },
+    { type: WalletType.erc20Viberate, name: "Viberate", ticker: "VIB" },
+    { type: WalletType.erc20Iconomi, name: "Iconomi", ticker: "ICN" },
+    { type: WalletType.erc20DTR, name: "Dynamic Trading Rights", ticker: "DTR" },
+    { type: WalletType.erc20OriginTrail, name: "OriginTrail", ticker: "TRAC" },
+    { type: WalletType.erc20InsurePal, name: "InsurePal", ticker: "IPL" },
+    { type: WalletType.erc20Xaurum, name: "Xaurum", ticker: "XAURUM" },
+    { type: WalletType.erc20Tron, name: "Tron", ticker: "TRX" },
+    { type: WalletType.erc20VeChain, name: "VeChain", ticker: "VEN" },
+    { type: WalletType.erc20Binance, name: "Binance", ticker: "BNB" },
+    { type: WalletType.erc20Icon, name: "Icon", ticker: "ICX" },
+    { type: WalletType.erc20OmiseGo, name: "OmiseGo", ticker: "OMG" },
+    { type: WalletType.erc20WaltonChain, name: "WaltonChain", ticker: "WTC" },
     { type: WalletType.bitCoinTest, name: "Bitcoin Test", ticker: "BTC-T" },
+    { type: WalletType.ethereumTest, name: "Ethereum Test", ticker: "ETH-T" },
     { type: WalletType.bitCoinCashTest, name: "Bitcoin Cash Test", ticker: "BCH-T" },
     { type: WalletType.liteCoinTest, name: "Litecoin Test", ticker: "LTC-T" },
+    { type: WalletType.dashTest, name: "Dash Test", ticker: "DASH-T" },
     { type: WalletType.dogeCoinTest, name: "Dogecoin Test", ticker: "DOGE-T" },
-    { type: WalletType.rippleTest, name: "Ripple Test", ticker: "XRP-T" },
-    { type: WalletType.ethereumTest, name: "Ethereum Test", ticker: "ETH-T" },
-    { type: WalletType.erc20BokkyTest, name: "Bokky Test", ticker: "BOKKY-T" },
+    { type: WalletType.erc20BokkyTest, name: "Bokky ERC 20 Test", ticker: "BOKKY-T" },
+    { type: WalletType.erc20SaltTest, name: "Salt Test", ticker: "SALT-T" },
+    { type: WalletType.erc20PolymathTest, name: "Polymath Test", ticker: "POLY-T" },
+    { type: WalletType.erc200xTest, name: "0X Test", ticker: "ZRX-T" },
+    { type: WalletType.erc20CindicatorTest, name: "Cindicator Test", ticker: "CND-T" },
+    { type: WalletType.erc20CargoXTest, name: "CargoX Test", ticker: "CXO-T" },
+    { type: WalletType.erc20ViberateTest, name: "Viberate Test", ticker: "VIB-T" },
+    { type: WalletType.erc20IconomiTest, name: "Iconomi Test", ticker: "ICN-T" },
+    { type: WalletType.erc20DTRTest, name: "Dynamic Trading Rights Test", ticker: "DTR-T" },
+    { type: WalletType.erc20OriginTrailTest, name: "OriginTrail Test", ticker: "TRAC-T" },
+    { type: WalletType.erc20InsurePalTest, name: "InsurePal Test", ticker: "IPL-T" },
+    { type: WalletType.erc20XaurumTest, name: "Xaurum Test", ticker: "XAURUM-T" },
+    { type: WalletType.erc20TronTest, name: "Tron Test", ticker: "TRX-T" },
+    { type: WalletType.erc20VeChainTest, name: "VeChain Test", ticker: "VEN-T" },
+    { type: WalletType.erc20BinanceTest, name: "Binance Test", ticker: "BNB-T" },
+    { type: WalletType.erc20IconTest, name: "Icon Test", ticker: "ICX-T" },
+    { type: WalletType.erc20OmiseGoTest, name: "OmiseGo Test", ticker: "OMG-T" },
+    { type: WalletType.erc20WaltonChainTest, name: "WaltonChain Test", ticker: "WTC-T" },
 ];
 var BCDataRefreshStatusCode;
 (function (BCDataRefreshStatusCode) {
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,7 @@
 
 export interface HttpResponse{
     readonly status:number;
-    readonly body:BCHttpResponse|string|object;
+    readonly body:BCHttpResponse | string | object;
   
   }
   export interface BCHttpResponse{
@@ -46,7 +46,7 @@ export interface HttpResponse{
      * @description BCHttpResponse !== undefined if the request succeeded but the device returned an error code. 
      */
     BCHttpResponse:BCHttpResponse
-    constructor(data:HttpResponse|BCHttpResponse,m:string="DaemonError") {
+    constructor(data:HttpResponse | BCHttpResponse,m:string="DaemonError") {
         super(m);
 
         // Set the prototype explicitly.
@@ -109,23 +109,57 @@ export interface HttpResponse{
     TESTNET:0x40000000
   }
   export enum WalletType{
-    bitCoin     =WalletTypeConstants.BTC,
-    bitCoinCash =WalletTypeConstants.BTC+1,
-    liteCoin    =WalletTypeConstants.BTC+2,
-    dash        =WalletTypeConstants.BTC+3,
-    dogeCoin    =WalletTypeConstants.BTC+4,
-    ripple      =WalletTypeConstants.BTC+5,
-    ethereum    =WalletTypeConstants.ETH,
-    erc20Bokky  =WalletTypeConstants.ETH|WalletTypeConstants.ERC20,
+    bitCoin               =WalletTypeConstants.BTC,
+    bitCoinCash           =WalletTypeConstants.BTC+1,
+    liteCoin              =WalletTypeConstants.BTC+2,
+    dash                  =WalletTypeConstants.BTC+3,
+    dogeCoin              =WalletTypeConstants.BTC+4,
+    ripple                =WalletTypeConstants.BTC+5,
+    ethereum              =WalletTypeConstants.ETH,
+    erc20Bokky            =WalletTypeConstants.ETH  | WalletTypeConstants.ERC20,
+    erc20Salt             =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+1,
+    erc20Polymath			    =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+2,
+    erc200x			          =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+3,
+    erc20Cindicator			  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+4,
+    erc20CargoX		    	  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+5,
+    erc20Viberate			    =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+6,
+    erc20Iconomi	    		=(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+7,
+    erc20DTR			        =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+8,
+    erc20OriginTrail			=(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+9,
+    erc20InsurePal		   	=(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+10,
+    erc20Xaurum	    	   	=(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+11,
+    erc20Tron		    	    =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+12,
+    erc20VeChain    			=(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+13,
+    erc20Binance    			=(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+14,
+    erc20Icon		    	    =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+15,
+    erc20OmiseGo    			=(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+16,
+    erc20WaltonChain			=(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)+17,
   
-    bitCoinTest     =(WalletTypeConstants.BTC) | WalletTypeConstants.TESTNET,
-    bitCoinCashTest =(WalletTypeConstants.BTC+1)| WalletTypeConstants.TESTNET,
-    liteCoinTest    =(WalletTypeConstants.BTC+2)| WalletTypeConstants.TESTNET,
-    dashTest        =(WalletTypeConstants.BTC+3)| WalletTypeConstants.TESTNET,
-    dogeCoinTest    =(WalletTypeConstants.BTC+4)| WalletTypeConstants.TESTNET,
-    rippleTest      =(WalletTypeConstants.BTC+5)| WalletTypeConstants.TESTNET,
-    ethereumTest    =(WalletTypeConstants.ETH)  | WalletTypeConstants.TESTNET,
-    erc20BokkyTest  =(WalletTypeConstants.ETH|WalletTypeConstants.ERC20)| WalletTypeConstants.TESTNET
+    bitCoinTest         =(WalletTypeConstants.BTC)  | WalletTypeConstants.TESTNET,
+    bitCoinCashTest     =(WalletTypeConstants.BTC+1)| WalletTypeConstants.TESTNET,
+    liteCoinTest        =(WalletTypeConstants.BTC+2)| WalletTypeConstants.TESTNET,
+    dashTest            =(WalletTypeConstants.BTC+3)| WalletTypeConstants.TESTNET,
+    dogeCoinTest        =(WalletTypeConstants.BTC+4)| WalletTypeConstants.TESTNET,
+    rippleTest          =(WalletTypeConstants.BTC+5)| WalletTypeConstants.TESTNET,
+    ethereumTest        =(WalletTypeConstants.ETH)  | WalletTypeConstants.TESTNET,
+    erc20BokkyTest      =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20)| WalletTypeConstants.TESTNET,
+    erc20SaltTest			  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+1,
+    erc20PolymathTest	  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+2,
+    erc200xTest			    =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+3,
+    erc20CindicatorTest =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+4,
+    erc20CargoXTest		  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+5,	
+    erc20ViberateTest	  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+6,
+    erc20IconomiTest	  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+7,
+    erc20DTRTest			  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+8,
+    erc20OriginTrailTest=(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+9,
+    erc20InsurePalTest  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+10,	
+    erc20XaurumTest		  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+11,
+    erc20TronTest			  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+12,
+    erc20VeChainTest	  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+13,
+    erc20BinanceTest	  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+14,
+    erc20IconTest			  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+15,
+    erc20OmiseGoTest	  =(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+16,
+    erc20WaltonChainTest=(WalletTypeConstants.ETH | WalletTypeConstants.ERC20 | WalletTypeConstants.TESTNET)+17,
   }
   export interface WalletTypeInfo{
     type:WalletType
@@ -133,21 +167,53 @@ export interface HttpResponse{
     ticker:string;
   }
   export const typeInfoMap:WalletTypeInfo[] = [
-    	{type:WalletType.bitCoin				,name:"Bitcoin",ticker:"BTC"} ,
-    	{type:WalletType.bitCoinCash		,name:"Bitcoin Cash",ticker:"BCH"} ,
-    	{type:WalletType.liteCoin				,name:"Litecoin",ticker:"LTC"} ,
-      {type:WalletType.dash					  ,name:"Dash",ticker:"DASH"} ,
-      {type:WalletType.dogeCoin			  ,name:"Dogecoin",ticker:"DOGE"} ,
-      {type:WalletType.ripple				  ,name:"Ripple",ticker:"XRP"} ,
-      {type:WalletType.ethereum			  ,name:"Ethereum",ticker:"ETH"} ,
-      {type:WalletType.erc20Bokky		  ,name:"Bokky",ticker:"BOKKY"} ,
-    	{type:WalletType.bitCoinTest		,name:"Bitcoin Test",ticker:"BTC-T"} ,
-    	{type:WalletType.bitCoinCashTest,name:"Bitcoin Cash Test",ticker:"BCH-T"} ,
-    	{type:WalletType.liteCoinTest		,name:"Litecoin Test",ticker:"LTC-T"} ,
-    	{type:WalletType.dogeCoinTest		,name:"Dogecoin Test",ticker:"DOGE-T"} ,
-    	{type:WalletType.rippleTest			,name:"Ripple Test",ticker:"XRP-T"} ,
-    	{type:WalletType.ethereumTest		,name:"Ethereum Test",ticker:"ETH-T"} ,
-    	{type:WalletType.erc20BokkyTest	,name:"Bokky Test",ticker:"BOKKY-T"} ,
+    {type:WalletType.bitCoin,name:"Bitcoin", ticker:"BTC"},
+    {type:WalletType.ethereum,name:"Ethereum", ticker:"ETH"},
+    {type:WalletType.bitCoinCash,name:"Bitcoin Cash", ticker:"BCH"},
+    {type:WalletType.liteCoin,name:"Litecoin", ticker:"LTC"},
+    {type:WalletType.dash,name:"Dash", ticker:"DASH"},
+    {type:WalletType.dogeCoin,name:"Dogecoin", ticker:"DOGE"},
+    {type:WalletType.erc20Salt,name:"Salt" ,ticker:"SALT"},
+    {type:WalletType.erc20Polymath,name:"Polymath" ,ticker:"POLY"},
+    {type:WalletType.erc200x,name:"0X" ,ticker:"ZRX"},
+    {type:WalletType.erc20Cindicator,name:"Cindicator" ,ticker:"CND"},
+    {type:WalletType.erc20CargoX,name:"CargoX" ,ticker:"CXO"},
+    {type:WalletType.erc20Viberate,name:"Viberate" ,ticker:"VIB"},
+    {type:WalletType.erc20Iconomi,name:"Iconomi" ,ticker:"ICN"},
+    {type:WalletType.erc20DTR,name:"Dynamic Trading Rights" ,ticker:"DTR"},
+    {type:WalletType.erc20OriginTrail,name:"OriginTrail" ,ticker:"TRAC"},
+    {type:WalletType.erc20InsurePal,name:"InsurePal" ,ticker:"IPL"},
+    {type:WalletType.erc20Xaurum,name:"Xaurum" ,ticker:"XAURUM"},
+    {type:WalletType.erc20Tron,name:"Tron" ,ticker:"TRX"},
+    {type:WalletType.erc20VeChain,name:"VeChain" ,ticker:"VEN"},
+    {type:WalletType.erc20Binance,name:"Binance" ,ticker:"BNB"},
+    {type:WalletType.erc20Icon,name:"Icon" ,ticker:"ICX"},
+    {type:WalletType.erc20OmiseGo,name:"OmiseGo" ,ticker:"OMG"},
+    {type:WalletType.erc20WaltonChain,name:"WaltonChain" ,ticker:"WTC"},
+    {type:WalletType.bitCoinTest,name:"Bitcoin Test", ticker:"BTC-T"},
+    {type:WalletType.ethereumTest,name:"Ethereum Test" ,ticker:"ETH-T"},
+    {type:WalletType.bitCoinCashTest,name:"Bitcoin Cash Test", ticker:"BCH-T"},
+    {type:WalletType.liteCoinTest,name:"Litecoin Test", ticker:"LTC-T"},
+    {type:WalletType.dashTest,name:"Dash Test", ticker:"DASH-T"},
+    {type:WalletType.dogeCoinTest,name:"Dogecoin Test", ticker:"DOGE-T"},
+    {type:WalletType.erc20BokkyTest,name:"Bokky ERC 20 Test" ,ticker:"BOKKY-T"},
+    {type:WalletType.erc20SaltTest,name:"Salt Test" ,ticker:"SALT-T"},
+    {type:WalletType.erc20PolymathTest,name:"Polymath Test" ,ticker:"POLY-T"},
+    {type:WalletType.erc200xTest,name:"0X Test" ,ticker:"ZRX-T"},
+    {type:WalletType.erc20CindicatorTest,name:"Cindicator Test" ,ticker:"CND-T"},
+    {type:WalletType.erc20CargoXTest,name:"CargoX Test" ,ticker:"CXO-T"},
+    {type:WalletType.erc20ViberateTest,name:"Viberate Test" ,ticker:"VIB-T"},
+    {type:WalletType.erc20IconomiTest,name:"Iconomi Test" ,ticker:"ICN-T"},
+    {type:WalletType.erc20DTRTest,name:"Dynamic Trading Rights Test" ,ticker:"DTR-T"},
+    {type:WalletType.erc20OriginTrailTest,name:"OriginTrail Test" ,ticker:"TRAC-T"},
+    {type:WalletType.erc20InsurePalTest,name:"InsurePal Test" ,ticker:"IPL-T"},
+    {type:WalletType.erc20XaurumTest,name:"Xaurum Test" ,ticker:"XAURUM-T"},
+    {type:WalletType.erc20TronTest,name:"Tron Test" ,ticker:"TRX-T"},
+    {type:WalletType.erc20VeChainTest,name:"VeChain Test" ,ticker:"VEN-T"},
+    {type:WalletType.erc20BinanceTest,name:"Binance Test" ,ticker:"BNB-T"},
+    {type:WalletType.erc20IconTest,name:"Icon Test" ,ticker:"ICX-T"},
+    {type:WalletType.erc20OmiseGoTest,name:"OmiseGo Test" ,ticker:"OMG-T"},
+    {type:WalletType.erc20WaltonChainTest,name:"WaltonChain Test" ,ticker:"WTC-T"},
   ];
 
   export interface BCObject{
